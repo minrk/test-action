16193197a6858d66cae4079d7e6acd427921c02c
Min RK <benjaminrk@gmail.com>
2021-04-16 12:58:15 +0200

    use octokit to make requests
    who knows what's up with direct requests

--- index.js
+++ index.js
@@ -2,33 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const https = require("https");
 
-const token = process.env["ACTIONS_RUNTIME_TOKEN"];
-console.log(`Got token without asking for it: ${token.slice(0, 10)}...`);
+async function run() {
+  const token = process.env["ACTIONS_RUNTIME_TOKEN"];
+  console.log(`Got token without asking for it: ${token.slice(0, 10)}...`);
 
-console.log("env:");
-console.log(process.env);
+  console.log("env:");
+  console.log(process.env);
 
-const options = {
-  hostname: "api.github.com",
-  port: 443,
-  path: "/user",
-  method: "GET",
-  headers: {
-    Authorization: `Bearer ${token}`,
-    "User-Agent": "my-github-action",
-  },
-};
+  const octokit = github.getOctokit(token);
 
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-  console.log(`headers ${res.headers}`);
+  const { data, headers } = await octokit.request("/user");
+  console.log(data);
+  console.log(headers);
+}
 
-  res.on("data", (d) => {
-    process.stdout.write(d);
-  });
-});
-
-req.on("error", (error) => {
-  console.error(`error! ${error}`);
-  core.setFailed(error.message);
-});
+run();
