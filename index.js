fa2e33b0a2bda4d54c4afd6f6874a8dfe4f92663
Min RK <benjaminrk@gmail.com>
2021-04-16 13:08:00 +0200

    back to input token

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const github = require("@actions/github");
 const https = require("https");
 
 async function run() {
-  const token = process.env["ACTIONS_RUNTIME_TOKEN"];
-  console.log(`Got token without asking for it: ${token.slice(0, 10)}...`);
+  const token = core.getInput("token");
+  console.log(`Got token: ${token.slice(0, 10)}...`);
+  const env = core.getInput("env");
+  console.log(`Got env: ${env}`);
 
-  console.log("env:");
+  console.log("process env:");
   console.log(process.env);
 
   const octokit = github.getOctokit(token);
 
   try {
-      const { data, headers } = await octokit.request("/");
-  console.log(data);
-  console.log(headers);
+    const { data, headers } = await octokit.request("/user");
+    console.log(data);
+    console.log(headers);
   } catch (e) {
     console.log(e);
     core.setFailed(e.message);
