684bcaf14f1896165401ff2d20c2cc0e86b437bd
Min RK <benjaminrk@gmail.com>
2021-04-16 12:36:33 +0200

    try without asking for any permissions

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const https = require("https");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const token = core.getInput('token');
-  const secret = core.getInput('secret');
-  if (token) {
-      console.log(`Got token: ${token.slice(0, 3)}...`)
-  } else {
-      console.log("Got no token");
-  }
-  if (secret) {
-      console.log(`Got secret: ${secret.slice(0, 3)}...`)
-  } else {
-      console.log("Got no secret");
-  }
-  core.setOutput("token", token.slice(0, 5));
-  core.setOutput("secret", secret.slice(0, 5));
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log(process.env);
-} catch (error) {
+const token = process.env["ACTIONS_RUNTIME_TOKEN"];
+console.log(`Got token without asking for it: ${token.slice(0, 10)}...`);
+
+console.log("env:");
+console.log(process.env);
+
+const options = {
+  hostname: "api.github.com",
+  port: 443,
+  path: "/user",
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "User-Agent": "my-github-action",
+  },
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+  console.log(`headers ${res.headers}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(`error! ${error}`);
   core.setFailed(error.message);
-}
+});
